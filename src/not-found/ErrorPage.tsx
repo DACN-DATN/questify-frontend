966ae933798097b129fd2ecd575fa59c0ef26387
HuyyTran <huytrn.dev@gmail.com>
2025-03-02 00:55:43 +0700

    local img add

--- src/not-found/ErrorPage.tsx
+++ src/not-found/ErrorPage.tsx
@@ -4,8 +4,6 @@ import Header from '../components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 
 const ErrorPage: React.FC = () => {
-  const supportLinks = ['FAQs', 'Privacy Policy', 'Terms & Condition'];
-
   return (
     <div className={styles.errorPage}>
       <Header />
@@ -22,12 +20,7 @@ const ErrorPage: React.FC = () => {
           <button className={styles.goBackButton}>Go Back</button>
         </div>
         <div className={styles.imageWrapper}>
-          <img
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/db6347c0f60b459dadd5502145d0d94e/31f25e5f7b4e06ed69b691e4becda73206d74b6f241820be244dd74491be04b2?apiKey=db6347c0f60b459dadd5502145d0d94e&"
-            className={styles.backgroundImage}
-            alt=""
-          />
+          <img loading="lazy" src="/404.png" className={styles.backgroundImage} alt="" />
         </div>
       </div>
       <Footer />
